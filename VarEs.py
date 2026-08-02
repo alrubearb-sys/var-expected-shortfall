3c26c340eac9403b5386a5bd771bcb07614dc04a
Alejandro Ruz <alrube.arb@gmail.com>
2026-08-02 23:26:37 +0200

    hoja de ruta creada

--- VarEs.py
+++ VarEs.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 import pandas
 
-
+# prueba 
 datos = yf.download("AAPL", period="2y")
 precios = datos["Close"]
 ticker = precios["AAPL"]
@@ -14,8 +14,16 @@ print(rendimientos)
 # funcion para extraer los retornos de las compañias en el periodo de dos años
 
 def get_returns(ticker):
-    datos = yf.download("AAPL", period="2y")
-    
+    datos = yf.download(ticker, period="2y")
+
+## Var Historico
+# obtener rendimientos diarios de cada compañia en los ultimos dos años
+# ordenar rendiminetos de peor a mejor (pandas)
+# eleccion nivel de confianza (95/99)
+
+
+## VaR Expected Shortfall
+# sacar la media de los rendimientos mas alla del VaR
 
 
 
